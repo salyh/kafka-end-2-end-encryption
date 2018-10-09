b6b9a893611bc6d75e5f10e4108e26f1c7325421
Sven Pfennig <sven.pfennig@alvary.io>
2018-10-09 16:42:50 +0200

    Replace DatatypeConverter.printHexBinary() with bytesToHex() from StackOverflow

--- src/main/java/de/saly/kafka/crypto/SerdeCryptoBase.java
+++ src/main/java/de/saly/kafka/crypto/SerdeCryptoBase.java
@@ -23,7 +23,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
 
 import org.apache.kafka.common.KafkaException;
 import org.apache.kafka.common.utils.Utils;
@@ -73,7 +72,7 @@ public abstract class SerdeCryptoBase {
                     final byte rsaFactor = encrypted[3];
                     final byte ivLen = encrypted[4];
                     final int offset = HEADER_LENGTH + hashLen + (rsaFactor * RSA_MULTIPLICATOR) + ivLen;
-                    final String aesHash = DatatypeConverter.printHexBinary(Arrays.copyOfRange(encrypted, HEADER_LENGTH, HEADER_LENGTH + hashLen));
+                    final String aesHash = bytesToHex(Arrays.copyOfRange(encrypted, HEADER_LENGTH, HEADER_LENGTH + hashLen));
                     final byte[] iv = Arrays.copyOfRange(encrypted, HEADER_LENGTH + hashLen + (rsaFactor * RSA_MULTIPLICATOR),
                             HEADER_LENGTH + hashLen + (rsaFactor * RSA_MULTIPLICATOR) + ivLen);
 
@@ -101,6 +100,22 @@ public abstract class SerdeCryptoBase {
                 throw new KafkaException("Decrypt failed",e);
             }
         }
+    }
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    /**
+     * BytesToHex from https://stackoverflow.com/a/9855338
+     * @param bytes Byte array that should be converted to hex string
+     * @return String that contains two chars (0-9,A-F) for each byte
+     */
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
     }
 
     private class ThreadAwareKeyInfo {
